36e71da29dd8d3ad077f221f7c9544873c595163
MTcloud2023 <admon.montajes.t@gmail.com>
2026-03-05 14:29:26 -0600

    ZxzX

--- web/settings.py
+++ web/settings.py
@@ -118,6 +118,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
+
+INSTALLED_APPS += [
+    "cloudinary",
+    "cloudinary_storage",
+]
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+MEDIA_URL = "/media/"
